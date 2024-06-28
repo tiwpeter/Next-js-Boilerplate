63d0558b7d590eb50ef7d89edc8745618422ed22
Tiepeter777 <idkaimak02@gmail.com>
2024-06-29 06:10:54 +0700

    7.2click

--- src/app/[locale]/(unauth)/layout.tsx
+++ src/app/[locale]/(unauth)/layout.tsx
@@ -21,42 +21,35 @@ export default function Layout(props: { children: React.ReactNode }) {
     <BaseTemplate
       leftNav={
         <>
-          <div>
-            <i 
-              className="material-icons icon-va-8 p-lnr-8 cursor-pointer flex"
-              onClick={toggleMenu}
-            >
-              menu
-            </i>
-          </div>
-          <div className={`bg-red-500 ${isMenuOpen ? 'w-299' : 'w-5'}`}>
-            <ul className="pl-0">
-            <li className={`${isMenuOpen ? 'flex items-center gap-2 mb-2' : ''}`}>
-  <span className="icon">📌</span>
-  {isMenuOpen ? (
-    <Link href="/" passHref>
-      <span
-        className="border-none text-gray-700 hover:text-gray-900 block py-2"
-        onClick={closeMenu}
-      >
-        {t('home_link')}
-      </span>
-    </Link>
-  ) : null}
-</li>
-              <li>
-                <span className="icon">📌</span>
-                {isMenuOpen ? (
-                  <Link
-                    href="/about/"
-                    className="border-none text-gray-700 hover:text-gray-900"
-                    onClick={closeMenu}
-                  >
-                    {t('about_link')}
-                  </Link>
-                ) : null}
-              </li>
-            </ul>
+          <div className="bg-gray-300 w-60 h-80">
+            <div>
+              <i
+                className="material-icons icon-va-8 p-lnr-8 cursor-pointer flex"
+                onClick={toggleMenu}
+              >
+                menu
+              </i>
+            </div>
+            {/* menu */}
+            <div className={`bg-red-500 ${isMenuOpen ? 'w-299' : 'w-10'}`}>
+              <ul className="pl-0">
+                <li className={`${isMenuOpen ? 'flex items-center gap-2 mb-2' : ''}`}>
+                  <span className="icon">📌</span>
+                  {isMenuOpen ? (
+                    <Link href="/" passHref>
+                      <span
+                        className="border-none text-gray-700 hover:text-gray-900 block py-2"
+                        onClick={closeMenu}
+                      >
+                        {t('home_link')}
+                      </span>
+                    </Link>
+                  ) : (
+                    <span>Home</span>
+                  )}
+                </li>
+              </ul>
+            </div>
           </div>
         </>
       }
@@ -65,3 +58,35 @@ export default function Layout(props: { children: React.ReactNode }) {
     </BaseTemplate>
   );
 }
+/*
+leftNav={
+  <>
+    <div>
+      <i 
+        className="material-icons icon-va-8 p-lnr-8 cursor-pointer flex"
+        onClick={toggleMenu}
+      >
+        menu
+      </i>
+    </div>
+   
+</Link>
+) : null}
+</li>
+        <li>
+          <span className="icon">📌</span>
+          {isMenuOpen ? (
+            <Link
+              href="/about/"
+              className="border-none text-gray-700 hover:text-gray-900"
+              onClick={closeMenu}
+            >
+              {t('about_link')}
+            </Link>
+          ) : null}
+        </li>
+      </ul>
+    </div>
+  </>
+}
+>*/
