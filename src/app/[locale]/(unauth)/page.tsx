aa3d949bd7b9d512135371d7148e1679d57a4d84
Tiepeter777 <idkaimak02@gmail.com>
2024-06-29 09:09:21 +0700

    up

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -18,13 +18,19 @@ export default function Index() {
   return (
     <>
       <div className='bg-red-500' style={{ width: '1080px', height:'185px' }}>
-        <div className='border border-gray-500 rounded-lg p-4'>
-          <h1>home</h1>
+        <div className='border border-gray-500 rounded-lg p-2'>
+          <h1>Announce</h1>
         </div>
+        {/**
+         border เพิ่มเส้นขอบให้กับ div
+border-gray-500 กำหนดสีของเส้นขอบเป็นสีเทา (สามารถเปลี่ยนสีตามต้องการ)
+rounded-lg ทำให้ขอบของ div มีความโค้งมน
+p-4 เพิ่ม padding รอบ ๆ เนื้อหาใน div
+         */}
         <ul>
-          <li className='border border-gray-500 rounded-lg p-4'>asdas</li>
-          <li className='border border-gray-500 rounded-lg p-4'>asdas</li>
-          <li className='border border-gray-500 rounded-lg p-4'>asdas</li>
+          <li className='border border-gray-500 rounded-lg p-2'>asdas</li>
+          <li className='border border-gray-500 rounded-lg p-2'>asdas</li>
+          <li className='border border-gray-500 rounded-lg p-2'>asdas</li>
         </ul>
       </div>
       
