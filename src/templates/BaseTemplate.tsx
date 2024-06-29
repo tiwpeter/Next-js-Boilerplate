5ac585efeed7c1fe37d6fe0a877fc01593c7ccf7
Tiepeter777 <idkaimak02@gmail.com>
2024-06-29 08:19:06 +0700

    input

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -15,25 +15,45 @@ const BaseTemplate = (props: {
     setIsMenuOpen(!isMenuOpen);
   };
 /* max-w-screen-md */
+{/**ml-2 (margin-left 2) และ mr-2 (margin-right 2)  */}
   return (
     <div className="w-full px-1 text-gray-700 antialiased">
-      <div className="mx-auto">
-        <header className="border-b border-gray-300">
-          <div className="pb-8 pt-16">
-            <h1 className="text-3xl font-bold text-gray-900">
-              {AppConfig.name}
-            </h1>
-            <div>
-              <i
-                className="material-icons icon-va-8 p-lnr-8 cursor-pointer"
-                onClick={toggleMenu}
-              >
-                menu
-              </i>
-            </div>
-            <h2 className="text-xl">{t('description')}</h2>
-          </div>
-        </header>
+    <div className="mx-auto">
+      <div className="border-b border-gray-500 flex items-center space-x-4 py-2 justify-center">
+        <div className='flex items-center'>
+          <p className="ml-0">menu</p>
+        </div>
+        <div className='flex items-center'>
+          <p className="ml-0">pantip</p>
+        </div>
+        <div className='flex items-center ml-auto'>
+        <input
+            type="text"
+            className="border border-gray-400 rounded 
+            ml-60 mr-60
+            w-96 h-7
+            "
+          />
+        </div>
+        <div className='flex items-center'>
+          <p>ตั้งกระทู้</p>
+        </div>
+        <div className='flex items-center'>
+          <span className="flex space-x-1 ml-2">
+            <span>เข้าสู่ระบบ</span>
+            <span>/</span>
+            <span>สมัครสมาชิค</span>
+          </span>
+        </div>
+        </div>
+
+
+{/*Nav */}
+
+
+
+
+
 
         <main className="bg-blue-200 flex h-screen">
 
