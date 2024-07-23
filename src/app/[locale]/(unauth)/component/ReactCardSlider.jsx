241c07576f36022c857ac301c9c8a1e34215b3e9
Tiepeter777 <idkaimak02@gmail.com>
2024-07-23 17:31:42 +0700

    linksliewaitadjustcss

--- src/app/[locale]/(unauth)/component/ReactCardSlider.jsx
+++ src/app/[locale]/(unauth)/component/ReactCardSlider.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Slider.css';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import Link from 'next/link';
 
 const ReactCardSlider = (props) => {
     const [isAtStart, setIsAtStart] = useState(true);
@@ -46,11 +47,13 @@ const ReactCardSlider = (props) => {
                 {
                     props.slides.map((slide, index) => {
                         return (
-                            <div className="slider-card" key={index} onClick={() => slide.clickEvent()}>
-                                <div className="slider-card-image" style={{ backgroundImage: `url(${slide.image})`, backgroundSize: 'cover' }}></div>
-                                <p className="slider-card-title">{slide.title}</p>
-                                <p className="slider-card-description">{slide.description}</p>
-                            </div>
+                            <Link href={slide.link} key={index} legacyBehavior>
+                                <a className="slider-card">
+                                    <div className="slider-card-image" style={{ backgroundImage: `url(${slide.image})`, backgroundSize: 'cover' }}></div>
+                                    <p className="slider-card-title">{slide.name}</p>
+                                    <p className="slider-card-description">{slide.description}</p>
+                                </a>
+                            </Link>
                         )
                     })
                 }
--- src/app/[locale]/(unauth)/layout.tsx
+++ src/app/[locale]/(unauth)/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from 'react';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
@@ -7,50 +7,29 @@ import { BaseTemplate } from '@/templates/BaseTemplate';
 import ReactCardSlider from './component/ReactCardSlider';
 import './App.css';
 
-
 export default function Layout(props: { children: React.ReactNode }) {
   const t = useTranslations('RootLayout');
   const [activeLink, setActiveLink] = useState('/');
 
   const data = [
-    { name: t('home_link'), link: '/',image:"https://picsum.photos/200/300" },
-    { name: t('about_link'), link: '/about/',image:"https://picsum.photos/200/300" },
-    { name: 'Room', link: '/room/',image:"https://picsum.photos/200/300" },
-    { name: 'dw', link: '/room/',image:"https://picsum.photos/600/500" },
-    { name: 'Icon', link: '/room/',image:"https://picsum.photos/600/500" }
-
+    { name: t('home_link'), link: '/', image: "https://picsum.photos/200/300" },
+    { name: t('about_link'), link: '/about/', image: "https://picsum.photos/200/300" },
+    { name: 'Room', link: '/room/', image: "https://picsum.photos/200/300" },
+    { name: 'dw', link: '/room/', image: "https://picsum.photos/600/500" },
+    { name: 'Icon', link: '/room/', image: "https://picsum.photos/600/500" }
   ];
 
   return (
-    <div id="body">
-      <ReactCardSlider slides={data}/>
-  </div>
-  );
-}
-
-/*
- <BaseTemplate
+    <BaseTemplate
       leftNav={
         <>
-          {data.map((item, index) => (
-            <li key={index}>
-              <Link href={item.link} legacyBehavior>
-                <a
-                  onClick={() => setActiveLink(item.link)}
-                  className={`border-none text-gray-700 hover:text-gray-900 ${
-                    activeLink === item.link ? 'underline' : ''
-                  }`}
-                >
-                  {item.name}
-                </a>
-              </Link>
-            </li>
-          ))}
+          <div id="body">
+            <ReactCardSlider slides={data} />
+          </div>
         </>
       }
     >
       <div className="py-5 text-xl [&_p]:my-6">{props.children}</div>
     </BaseTemplate>
-
-
-*/
+  );
+}
