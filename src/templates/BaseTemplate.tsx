011a6db73c3f0bcb8224153efb926d891b7a5889
Tiepeter777 <idkaimak02@gmail.com>
2024-07-23 17:43:15 +0700

    linksliewaitadjustcss2

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -13,17 +13,13 @@ const BaseTemplate = (props: {
     <div className="w-full px-1 text-gray-700 antialiased">
       <div className="mx-auto max-w-screen-md">
         <header className="border-b border-gray-300">
-          <div className="pb-8 pt-16">
-            <h1 className="text-3xl font-bold text-gray-900">
-              {AppConfig.name}
-            </h1>
-            <h2 className="text-xl">{t('description')}</h2>
-          </div>
+         
 
           <div className="flex justify-between">
             <nav>
               <ul className="flex flex-wrap gap-x-5 text-xl">
-                {props.leftNav}
+             {props.leftNav}
+                dw
               </ul>
             </nav>
 
@@ -35,6 +31,7 @@ const BaseTemplate = (props: {
           </div>
         </header>
 
+        {/**show detail */}
         <main>{props.children}</main>
 
         <footer className="border-t border-gray-300 py-8 text-center text-sm">
