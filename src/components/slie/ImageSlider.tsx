a5db2f4a8478ff3ec2737ec09dfae9aab5baa744
Tiepeter777 <idkaimak02@gmail.com>
2024-07-26 12:04:58 +0700

    gradientslie2

--- src/components/slie/ImageSlider.tsx
+++ src/components/slie/ImageSlider.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Slider.css';
 import './buttoncolor.css'
-
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import Link from 'next/link';
 
@@ -11,25 +10,32 @@ const ReactCardSlider = (props) => {
 
     const slideLeft = () => {
         var slider = document.getElementById("slider");
-        slider.scrollLeft = slider.scrollLeft - 500;
+        slider.scrollLeft -= 500;
     };
 
     const slideRight = () => {
         var slider = document.getElementById("slider");
-        slider.scrollLeft = slider.scrollLeft + 500;
+        slider.scrollLeft += 500;
     };
 
     const checkScrollPosition = () => {
         var slider = document.getElementById("slider");
+        const sliderContainer = document.querySelector('.sliecolor');
+
         if (slider.scrollLeft === 0) {
             setIsAtStart(true);
+            sliderContainer.classList.add('hidden-left');
         } else {
             setIsAtStart(false);
+            sliderContainer.classList.remove('hidden-left');
         }
+
         if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
             setIsAtEnd(true);
+            sliderContainer.classList.add('hidden-right');
         } else {
             setIsAtEnd(false);
+            sliderContainer.classList.remove('hidden-right');
         }
     };
 
@@ -43,26 +49,24 @@ const ReactCardSlider = (props) => {
     return (
         <div id="main-slider-container">
             {!isAtStart && (
-              <div >
+              <div>
                 <MdChevronLeft size={40} className="slider-icon left" onClick={slideLeft} />
               </div>
             )}
             <div className='sliecolor' id="slider">
                 {
-                    props.slides.map((slide, index) => {
-                        return (
-                            <Link href={slide.link} key={index} legacyBehavior>
-                                <a className="slider-card">
-                                    <div className="slider-card-image" style={{ backgroundImage: `url(${slide.image})`, backgroundSize: 'cover' }}></div>
-                                    <p className="slider-card-title">{slide.name}</p>
-                                </a>
-                            </Link>
-                        )
-                    })
+                    props.slides.map((slide, index) => (
+                        <Link href={slide.link} key={index} legacyBehavior>
+                            <a className="slider-card">
+                                <div className="slider-card-image" style={{ backgroundImage: `url(${slide.image})`, backgroundSize: 'cover' }}></div>
+                                <p className="slider-card-title">{slide.name}</p>
+                            </a>
+                        </Link>
+                    ))
                 }
             </div>
             {!isAtEnd && (
-              <div >
+              <div>
                 <MdChevronRight size={40} className="slider-icon right" onClick={slideRight} />
               </div>
             )}
